bf54ed9b2abfe5c08c75ca4c0d6598727580aa7e
Hugues Cassé <hugues.casse@laposte.net>
2016-03-07 13:16:59 +0100

    Added documentation.

--- make.py
+++ make.py
@@ -2,7 +2,7 @@
 from elfmake import *
 import elfmake.config as config
 
-goal("doc", ["doc/manual.thot"], "cd doc; ../../thot/thot.py manual.thot")
+goal("doc", ["doc/manual.thot"], "cd doc; ../../thot/thot.py manual.thot -DHTML_TEMPLATE=theme/template.html")	# -DHTML_ONE_FILE_PER=section")
 
 goal("autodoc", [],
 	[
